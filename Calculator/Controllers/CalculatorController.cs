f528965a5aed24cb17a98fdb1e354f797ab1dfbb
shubhamdhamane <A00257743@mycambrian.ca>
2022-10-14 16:11:13 -0400

    api addition added

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatorFunc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class CalculatorController : Controller
+    {
+        double result = 0;
+
+
+        [HttpGet]
+        public double Addition(double num1,double num2)
+        {
+            return CalculatorFunc.Calculator.Add(num1, num2);
+        }
+    }
+
+}
+
