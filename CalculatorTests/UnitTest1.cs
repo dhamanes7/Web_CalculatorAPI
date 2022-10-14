69b9868cd17ebe671906b0bc3e70db3246a457fa
shubhamdhamane <A00257743@mycambrian.ca>
2022-10-14 15:19:14 -0400

    AddTest is added

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -1,10 +1,16 @@
-﻿namespace CalculatorTests;
+﻿using CalculatorFunc;
+namespace CalculatorTests;
 
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(3,4,7)]
+    [DataRow(7.2, 5.4, 12.6)]
+    [DataRow(9, -3, 6)]
+    public void AdditionTest(double num1, double num2, double expected)
     {
+        var result = Calculator.Add(num1, num2);
+        Assert.AreEqual(expected, result);
     }
 }
