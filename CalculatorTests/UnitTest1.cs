ebc01e42a60768e2cb8d8cb3c0f731e23172435a
shubhamdhamane <A00257743@mycambrian.ca>
2022-10-14 16:05:18 -0400

    DynamicData added into all 4 tests

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using CalculatorFunc;
+﻿using System.Collections.Generic;
+using CalculatorFunc;
 namespace CalculatorTests;
 
 [TestClass]
@@ -6,8 +7,9 @@ public class UnitTest1
 {
     [TestMethod]
     [DataRow(3,4,7)]
-    [DataRow(7.2, 5.4, 12.6)]
+    [DataRow(7.2, 5, 12.2)]
     [DataRow(9, -3, 6)]
+    [DynamicData(nameof(GetNumbersToAdd),DynamicDataSourceType.Method)]
     public void AdditionTest(double num1, double num2, double expected)
     {
         var result = Calculator.Add(num1, num2);
@@ -16,8 +18,9 @@ public class UnitTest1
 
     [TestMethod]
     [DataRow(6, 4, 2)]
-    [DataRow(7.2, 5.4, 1.8)]
+    [DataRow(7.2, 5, 2.2)]
     [DataRow(9, -3, 12)]
+    [DynamicData(nameof(GetNumbersToSubtract), DynamicDataSourceType.Method)]
     public void SubtractionTest(double num1, double num2, double expected)
     {
         var result = Calculator.Subtract(num1, num2);
@@ -28,6 +31,7 @@ public class UnitTest1
     [DataRow(6, 4, 24)]
     [DataRow(7.2, 5.4, 38.88)]
     [DataRow(9, -3, -27)]
+    [DynamicData(nameof(GetNumbersToMultiply), DynamicDataSourceType.Method)]
     public void MultiplicationTest(double num1, double num2, double expected)
     {
         var result = Calculator.Multiply(num1, num2);
@@ -38,6 +42,7 @@ public class UnitTest1
     [DataRow(8, 4, 2)]
     [DataRow(7.2, 5, 1.44)]
     [DataRow(-9, 3, -3)]
+    [DynamicData(nameof(GetNumbersToDivide), DynamicDataSourceType.Method)]
     public void DivisionTest(double num1, double num2, double expected)
     {
         var result = Calculator.Division(num1, num2);
@@ -48,5 +53,78 @@ public class UnitTest1
     public void DivisionByZeroTest()
     {
         Assert.ThrowsException<DivideByZeroException>(() => Calculator.Division(2, 0));
+    }
+
+
+    private static IEnumerable<Object[]> GetNumbersToAdd()
+    {
+        var list = new List<Object[]>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Random random = new Random();
+            var num1 = random.NextDouble();
+            var num2 = random.NextDouble();
+            var expected = Calculator.Add(num1, num2);
+            list.Add(new Object[] { num1, num2, expected });
+
+        }
+
+        return list;
+    }
+
+    private static IEnumerable<Object[]> GetNumbersToSubtract()
+    {
+        var list = new List<Object[]>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Random random = new Random();
+            var num1 = random.NextDouble();
+            var num2 = random.NextDouble();
+            var expected = Calculator.Subtract(num1,num2);
+            list.Add(new Object[] { num1, num2, expected });
+
+        }
+
+        return list;
+    }
+
+    private static IEnumerable<Object[]> GetNumbersToMultiply()
+    {
+        var list = new List<Object[]>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Random random = new Random();
+            var num1 = random.NextDouble();
+            var num2 = random.NextDouble();
+            var expected = Calculator.Multiply(num1,num2);
+            list.Add(new Object[] { num1, num2, expected });
+
+        }
+
+        return list;
+    }
+
+    private static IEnumerable<Object[]> GetNumbersToDivide()
+    {
+        var list = new List<Object[]>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Random random = new Random();
+            var num1 = random.NextDouble();
+            var num2 = random.NextDouble();
+            if (num2 == 0)
+            {
+                num2 = 1;
+            }
+            var expected = Calculator.Division(num1,num2);
+            list.Add(new Object[] { num1, num2, expected });
+
+        }
+
+        return list;
     }
 }
